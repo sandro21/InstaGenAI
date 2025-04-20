3244e69ba42542ba12b28859cde276ff9ca3dcaa
Sandro <sandroqarqusashvili@gmail.com>
2025-04-19 20:44:19 -0400

    basic UI

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,13 @@ export const layout = {
   export default function RootLayout({ children }) {
     return (
       <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+            <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+             rel="stylesheet"
+        />
+      </head>
         <body>
           {children}
         </body>
--- app/page.js
+++ app/page.js
@@ -12,12 +12,18 @@ export default function Page() {
         <div className="input1">
           <label htmlFor="input1">Step 1: What is your account about?</label>
           <input id="input1" type="text" placeholder="Enter Your Niche:" />
-          <button type="submit" className="submit-button">Generate Username</button>
+          <button type="submit" className="submit-button">
+            <span className="gradient-text">Submit</span>
+          </button>
         </div>
       </div>
       <div className="right-panel">
-        <p>Step 2: Choose your favorite</p>
-        <div className='step2container'></div>
+        <label>Step 2: Choose your favorite</label>
+        <div className='step2container'>
+          <div className="username-card">@travelgram_vibes</div>
+           <div className="username-card">@fitflow_daily</div>
+            <div className="username-card">@aesthetic.reels</div>
+        </div>
       </div>
     </div>
   );
