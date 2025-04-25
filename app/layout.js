960e3fe4b1c44dbd285461faa86976eafbcd5c8b
Sandro <sandroqarqusashvili@gmail.com>
2025-04-24 22:06:47 -0400

    went back

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 // app/layout.js
-
 export const metadata = {
   title: 'InstaGen AI',
   description: 'AI-generated Instagram usernames',
@@ -8,8 +7,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
